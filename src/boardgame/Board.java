9d6254977c8e6c96dcdc2ce66bb1ebc1ab7a575f
Guilherme Leite Moraes <guilherme.lmoraes32@gmail.com>
2019-01-18 16:19:09 -0200

    add the boardException (RuntimeExcp. type), but have to insert the messages

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -5,7 +5,7 @@ public class Board {
 	private int rows;
 	private int colums;
 	private Piece[][] pieces;
-	
+
 	public Board(int rows, int colums) {
 		this.rows = rows;
 		this.colums = colums;
@@ -16,29 +16,32 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColums() {
 		return colums;
 	}
 
-	public void setColums(int colums) {
-		this.colums = colums;
-	}
-	
-	public Piece piece (int row, int column) {
+	public Piece piece(int row, int column) {
 		return pieces[row][column];
 	}
-	
-	public Piece piece (Position position) {
+
+	public Piece piece(Position position) {
 		return pieces[position.getRow()][position.getColumn()];
 	}
-	
+
 	public void placePiece(Piece piece, Position position) {
-		pieces[position.getRow()] [position.getColumn()] = piece;
+		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
-	
+
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < colums;
+	}
+
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+
+	public boolean thereIsAPiece(Position position) {
+		return piece(position) != null;
+	}
 }
