7815e910d93b3c4c7f827a7efbda040f3d5bd0d2
Guilherme Leite Moraes <guilherme.lmoraes32@gmail.com>
2019-02-13 08:07:38 -0200

    validated the pieces movements: you can't put yourself in check anymore. Plus

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -9,7 +9,7 @@ public class ChessPosition {
 	
 	public ChessPosition(char column, int row) {
 		if(column < 'a' || column > 'h' || row < 1 || row >8) {
-			throw new ChessException("Errir instantiagin ChessPosition. Valid values are from a1 to h8.");
+			throw new ChessException("Error instantiatin ChessPosition. Valid values are from a1 to h8.");
 		}
 		this.column = column;
 		this.row = row;
@@ -27,7 +27,7 @@ public class ChessPosition {
 		return new Position(8 - row, column - 'a');
 	}
 	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+		return new ChessPosition((char)('a' + position.getColumn()), 8 - position.getRow());
 	}
 	
 	@Override
